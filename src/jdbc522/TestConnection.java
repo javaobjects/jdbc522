7470de2157ce972b3810ac704c841274bc5da69c
yanbo0039 <yanbo0039@yeah.net>
2019-07-23 20:09:15 +0800

    第一步：加载驱动类

--- src/jdbc522/TestConnection.java
+++ src/jdbc522/TestConnection.java
@@ -4,6 +4,13 @@ public class TestConnection {
 
 	public static void main(String[] args) {
 
+
+		try {
+			//第一步：加载驱动类
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
